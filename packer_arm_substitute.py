f10962cb568d2c6e2d6cc0f3007793af3f6438b9
Matthew Westphall <westphall@wisc.edu>
2025-02-12 17:00:39 -0600

    Add file transfer/startup script to pexpect

--- packer_arm_substitute.py
+++ packer_arm_substitute.py
@@ -7,7 +7,8 @@
 #    location
 # 2. Start an automated VM install using that iso as a base, passing in a 
 #    kickstart file via extra kernel args using libvirt
-# 3. Attempt to log into the guest
+# 3. Transfer files from the host to the guest in some way (not actually sure how packer does this, virtiofs is used here)
+# 4. Attempt to log into the guest, run configuration script based on transferred files
 
 
 import pexpect
@@ -71,8 +72,10 @@ def launch_libvirt_build(iso_path: Path, kickstart_path: Path, storage_pool: str
     """
     cmd = [
         'virt-install',
+        '--network', 'network=host-bridge,model=virtio',
         '--name', iso_path.name,
         '--disk', f'pool={storage_pool},size={img_size}',
+#        '--disk', f'path=/var/lib/libvirt/images/{iso_path.name}.img,size={img_size},format=raw',
         '--boot', 'uefi',
         '--initrd-inject', kickstart_path,
         f'--extra-args="inst.ks=file:/{kickstart_path.name}"',
@@ -116,12 +119,17 @@ def poll_libvirtd_progress(domain_name: str, sleep_interval: float = 5, timeout:
     
     print(f"VM build completed in {(datetime.now() - poll_start).seconds} seconds")
 
+def _pexpect_with_timeout(pexpect_proc: any, cmd: str):
+    outcome = pexpect_proc.expect([cmd, pexpect.TIMEOUT])
+    if outcome != 0:
+        raise RuntimeError(f"Timeout waiting for prompt '{cmd}' in pexpect")
 
-def pexpect_login_console(domain_name: str, password: str, login: str = 'root'):
+def pexpect_console_setup(domain_name: str, password: str, host_dev: str = 'host_home', cmd="cp /tmp/host-data/* ~", login: str = 'root'):
     ''' 
     Use pexpect to confirm that the regular bash login prompt eventually comes up
     when starting a post-install VM
     '''
+    print("Starting console login into VM")
     start_cmd = [
         'virsh',
         'start',
@@ -134,19 +142,26 @@ def pexpect_login_console(domain_name: str, password: str, login: str = 'root'):
         domain_name
     ]
 
-    pexpect_proc = pexpect.spawn(console_cmd[0], console_cmd[1:])
-    outcome = pexpect_proc.expect(['localhost login:', pexpect.TIMEOUT])
-    if outcome != 0:
-        raise RuntimeError("Timeout waiting for VM login prompt.")
+    pexpect_proc = pexpect.spawn(console_cmd[0], console_cmd[1:],timeout=60)
+
+    _pexpect_with_timeout(pexpect_proc, 'localhost login:')
     pexpect_proc.sendline(login)
-    outcome = pexpect_proc.expect(['Password:', pexpect.TIMEOUT])
-    if outcome != 0:
-        raise RuntimeError("Timeout waiting for VM password prompt.")
+
+    _pexpect_with_timeout(pexpect_proc, 'Password:')
     pexpect_proc.sendline(password)
+
+    print("Logged in successfully. Beginning setup.")
+    _pexpect_with_timeout(pexpect_proc, '#')
+    pexpect_proc.sendline("mkdir /tmp/host-data && mount -v -t virtiofs host_home /tmp/host-data")
+
+    _pexpect_with_timeout(pexpect_proc, '#')
+    pexpect_proc.sendline(cmd)
+
+    _pexpect_with_timeout(pexpect_proc, '#')
 
     pexpect_proc.close()
 
-    print(f"VM test login completed successfully.")
+    print(f"VM console setup completed successfully.")
 
     stop_cmd = [
         'virsh',
@@ -155,13 +170,29 @@ def pexpect_login_console(domain_name: str, password: str, login: str = 'root'):
     ]
     subprocess.call(stop_cmd)
 
-
-
-def dump_libvirt_xml(domain_name: str, output_path: Path):
+def configure_host_mount(domain_name: str, host_path: str):
     '''
-    Dump the libvirt xml definition of the created VM to allow replication of the VM
+    Configure a mount from the host to the guest by editing its XML
+    via the CLI
     '''
-    pass
+    # Enable shared memory
+    shm_cmd = [
+        'virt-xml',
+        domain_name,
+        '--edit',
+        '--memorybacking', 'source.type=memfd,access.mode=shared'
+    ]
+    subprocess.call(shm_cmd)
+
+    # Add a virtiofs device
+    virtiofs_cmd = [
+        'virt-xml',
+        domain_name,
+        '--add-device',
+        '--filesystem', f'driver.type=virtiofs,source.dir={host_path},target.dir=host_home'
+    ]
+    subprocess.call(virtiofs_cmd)
+
         
 def get_pw(pw_file: Path) -> str:
     '''
@@ -172,18 +203,29 @@ def get_pw(pw_file: Path) -> str:
 
     return password_data['password']
 
+CMD = (
+    "chmod +x /tmp/host-data/run-user-payload /tmp/host-data/osg-test.init && " 
+    "mv /tmp/host-data/run-user-payload /root/run-user-payload && "
+    "mv /tmp/host-data/osg-test.init /etc/osg-test.init && "
+    "mv /tmp/host-data/osg-test.service /etc/systemd/system/osg-test.service && "
+    "systemctl -q enable osg-test"
+)
+
 def main():
     cache_path = Path(argv[1])
     config_path = Path(argv[2])
     storage_pool = argv[3]
     password_file = Path(argv[4])
+    input_dir = Path(argv[5])
     iso_path = download_iso(cache_path, config_path / 'vars.json')
 
     domain_name = launch_libvirt_build(iso_path, config_path / 'kickstart.ks', storage_pool)
     poll_libvirtd_progress(domain_name)
 
+    configure_host_mount(domain_name, input_dir)
+
     password = get_pw(password_file)
-    pexpect_login_console(domain_name, password)
+    pexpect_console_setup(domain_name, password, cmd=CMD)
 
 if __name__ == '__main__':
     main()
